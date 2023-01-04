566b4723c9edd2818237f2f059a911b9436f3b68
Javi Carnero <javier@carnero.net>
2023-01-04 19:22:20 +0100

    scaffolding. basic eda.

---
+++ src/config.py
@@ -0,0 +1,7 @@
+DATA_STORAGE_PATH = "/run/media/jcarnero/linux-data"
+COMPETITION = "playground-series-s3e1"
+
+DATA_PATH = DATA_STORAGE_PATH + "/kaggle/" + COMPETITION
+INPUTS = DATA_PATH + "/input"
+PREPROCESSED = DATA_PATH + "/preprocess"
+OUTPUTS = DATA_PATH + "/output"
